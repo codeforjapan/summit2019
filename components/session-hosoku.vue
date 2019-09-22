b5bb6e7b1cb8b438244b5bf3efe65ae12263330a
URAMOTO Kazunori <kuprij@mac.com>
2019-09-22 22:08:30 +0900

    Safari でタイムテーブルの表示がおかしかったので、修正

--- components/session-hosoku.vue
+++ components/session-hosoku.vue
@@ -24,17 +24,20 @@ export default {
 </script>
 
 <style scoped>
-td.tt_hosoku {
-  filter: drop-shadow(5px 5px 5px rgba(0,0,0,.3));
-  color:#fff;
+td {
+  border: 1px solid #eee;
 }
-td.tt_hosoku div {
+
+td div {
   padding: 10px 9px !important;
   font-size: 12px;
-  border: 1px solid #eee;
-  background-color: #999;
   width:100% !important;
-  height:100% !important;
   margin:0 !important;
+}
+
+td.tt_hosoku {
+  background-color: #999;
+  filter: drop-shadow(5px 5px 5px rgba(0,0,0,.3));
+  color:#fff;
 }
 </style>
